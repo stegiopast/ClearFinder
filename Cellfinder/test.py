84b4b2f94dd1961d77cc615a7682896205d070ba
stegiopast <56259954+stegiopast@users.noreply.github.com>
2022-11-03 14:26:59 +0100

    Delete test.py

--- Cellfinder/test.py
+++
@@ -1,8 +0,0 @@
-import os
-if os.path.exists("/home/cellfinder_data/KF03/voxel_size_signal"):
-   with open("/home/cellfinder_data/KF03/voxel_size_signal/voxel_sizes.txt", "r") as file:
-       x = file.read().split(sep=",")
-       voxel_x = x[0]
-       voxel_y = x[1]
-       voxel_z = x[2]
-print(voxel_x,voxel_y,voxel_z)
